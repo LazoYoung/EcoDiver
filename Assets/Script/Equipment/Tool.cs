b593a9c1729ff8c82cec5f730f8a958efe61da1c
Chanyoung Park <idealproduction@naver.com>
2024-05-17 09:50:54 +0900

    #1 현재 사용중인 도구에 알맞은 Forearm 프리팹 적용

---
+++ Assets/Script/Equipment/Tool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Script.Equipment
+{
+    [CreateAssetMenu(fileName = "Tool", menuName = "Scriptable Object/Tool", order = 0)]
+    public class Tool : ScriptableObject
+    {
+        [SerializeField] private string toolName;
+        [SerializeField] private GameObject forearmPrefab;
+
+        public string Name => toolName;
+        public GameObject ForearmPrefab => forearmPrefab;
+    }
+}
