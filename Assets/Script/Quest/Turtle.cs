6f14cfd199ab3cbb0ad2cba01d1b1d038ef71d66
Chanyoung Park <idealproduction@naver.com>
2024-05-19 17:59:56 +0900

    #90 거북이 애니메이션 추가

--- Assets/Script/Quest/Turtle.cs
+++ Assets/Script/Quest/Turtle.cs
@@ -6,16 +6,51 @@ namespace Script.Quest
     [RequireComponent(typeof(Rigidbody))]
     public class Turtle : MonoBehaviour
     {
-        [SerializeField] public Sliceable fishingNet;
+        public Sliceable fishingNet;
+        public AnimationClip idleMotion;
+        public AnimationClip freeMotion;
+        private Animation _animation;
+        private string _clip;
+        private readonly string _idle = "idle";
+        private readonly string _free = "free";
 
         private void Start()
         {
             var body = GetComponent<Rigidbody>();
             body.freezeRotation = true;
+            _clip = _idle;
+            _animation = gameObject.AddComponent<Animation>();
             
             if (fishingNet == null)
             {
                 fishingNet = FindObjectOfType<Sliceable>();
+            }
+
+            if (fishingNet != null)
+            {
+                fishingNet.OnSlice += OnFree;
+            }
+
+            idleMotion.legacy = true;
+            freeMotion.legacy = true;
+            _animation.AddClip(idleMotion, _idle);
+            _animation.AddClip(freeMotion, _free);
+        }
+
+        private void Update()
+        {
+            if (_animation)
+            {
+                _animation.Play(_clip);
+            }
+        }
+
+        private void OnFree(Slicer slicer)
+        {
+            if (_animation)
+            {
+                _animation.Stop();
+                _clip = _free;
             }
         }
 
